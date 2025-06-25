d775e112f4cd637d7c6924e10cb4bdc1a1d3f708
lucas <lucasmartins308@gmail.com>
2025-06-25 15:27:16 -0300

    Atualizando o modelo Hospede: adicionando campos 'field' para todos os atributos e renomeando a tabela para 'Hospede', garantindo consistência na nomenclatura e melhorando a definição do modelo.

--- models/Hospede.js
+++ models/Hospede.js
@@ -5,54 +5,65 @@ const Hospede = sequelize.define('Hospede', {
   idHospede: {
     type: DataTypes.INTEGER,
     primaryKey: true,
-    autoIncrement: true
+    autoIncrement: true,
+    field: 'idHospede'
   },
   nome: {
     type: DataTypes.STRING(45),
-    allowNull: false
+    allowNull: false,
+    field: 'nome'
   },
   sobrenome: {
     type: DataTypes.STRING(45),
-    allowNull: false
+    allowNull: false,
+    field: 'sobrenome'
   },
   documento: {
     type: DataTypes.STRING(45),
-    allowNull: false
+    allowNull: false,
+    field: 'documento'
   },
   tipoDocumento: {
     type: DataTypes.STRING(45),
-    allowNull: false
+    allowNull: false,
+    field: 'tipoDocumento'
   },
   dtNascimento: {
     type: DataTypes.DATEONLY,
-    allowNull: false
+    allowNull: false,
+    field: 'dtNascimento'
   },
   telefone: {
     type: DataTypes.STRING(15),
-    allowNull: false
+    allowNull: false,
+    field: 'telefone'
   },
   email: {
     type: DataTypes.STRING(45),
-    allowNull: true
+    allowNull: true,
+    field: 'email'
   },
   genero: {
     type: DataTypes.STRING(1),
-    allowNull: false
+    allowNull: false,
+    field: 'genero'
   },
   preferencia: {
     type: DataTypes.STRING(45),
-    allowNull: true
+    allowNull: true,
+    field: 'preferencia'
   },
   Endereco_idEndereco: {
     type: DataTypes.INTEGER,
     allowNull: false,
+    field: 'Endereco_idEndereco',
     references: {
-      model: 'endereco',
+      model: 'Endereco',
       key: 'idEndereco'
     }
   }
 }, {
-  tableName: 'hospede',
+  tableName: 'Hospede',
   timestamps: false
 });
 
