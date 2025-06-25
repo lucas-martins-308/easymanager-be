2bafeef2c11152223b6874da3f54348f16704708
lucas <lucasmartins308@gmail.com>
2025-06-25 15:30:04 -0300

    Atualizando o modelo EstoqueItem: adicionando campos 'field' para todos os atributos e renomeando a tabela para 'EstoqueItem', garantindo consistência na nomenclatura e melhorando a definição do modelo.

--- models/EstoqueItem.js
+++ models/EstoqueItem.js
@@ -6,37 +6,44 @@ const EstoqueItem = sequelize.define('EstoqueItem', {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true,
+    field: 'idItem'
   },
   nomeItem: {
     type: DataTypes.STRING(45),
     allowNull: false,
+    field: 'nomeItem'
   },
   descricao: {
     type: DataTypes.STRING(45),
     allowNull: false,
+    field: 'descricao'
   },
   quantidade: {
     type: DataTypes.INTEGER,
     allowNull: false,
+    field: 'quantidade'
   },
   preco: {
     type: DataTypes.DECIMAL(10, 2),
     allowNull: false,
+    field: 'preco'
   },
   dtValidade: {
     type: DataTypes.DATEONLY,
     allowNull: true,
+    field: 'dtValidade'
   },
   Fornecedor_idFornecedor: {
     type: DataTypes.INTEGER,
     allowNull: false,
+    field: 'Fornecedor_idFornecedor',
     references: {
-      model: 'fornecedor',
+      model: 'Fornecedor',
       key: 'idFornecedor',
     },
   },
 }, {
-  tableName: 'estoqueitem',
+  tableName: 'EstoqueItem',
   timestamps: false,
 });
 
