21f03603d62cdebc0c30215ca9b2f022def71355
lucas <lucasmartins308@gmail.com>
2025-06-25 15:25:17 -0300

    Atualizando o modelo Fornecedor: adicionando campos 'field' para todos os atributos e renomeando a tabela para 'Fornecedor', garantindo consistência na nomenclatura e melhorando a definição do modelo.

--- models/Fornecedor.js
+++ models/Fornecedor.js
@@ -6,29 +6,34 @@ const Fornecedor = sequelize.define('Fornecedor', {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true,
+    field: 'idFornecedor'
   },
   nome: {
     type: DataTypes.STRING(45),
     allowNull: false,
+    field: 'nome'
   },
   telefone: {
     type: DataTypes.STRING(15),
     allowNull: false,
+    field: 'telefone'
   },
   email: {
     type: DataTypes.STRING(45),
     allowNull: false,
+    field: 'email'
   },
   Endereco_idEndereco: {
     type: DataTypes.INTEGER,
     allowNull: false,
+    field: 'Endereco_idEndereco',
     references: {
-      model: 'endereco',
+      model: 'Endereco',
       key: 'idEndereco',
     },
   },
 }, {
-  tableName: 'fornecedor',
+  tableName: 'Fornecedor',
   timestamps: false,
 });
 
